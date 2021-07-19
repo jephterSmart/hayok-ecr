a3db821ebcf9760b1f4a1c37f293a8e588a44089
Uzezi Jephter <kaskidsky@gmail.com>
2021-07-19 02:07:22 -0700

    worked on the postPatient controller

--- backend/app.js
+++ backend/app.js
@@ -4,32 +4,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const multer = require('multer');
+
 
 //routes to go into
 const authRoute = require('./routes/auth');
 const patientRoute = require('./routes/patient');
 
 const app = express();
-const fileStorage = multer.diskStorage({
-    destination: function(req,file,cb){
-        cb(null, 'images/')
-    },
-    filename: function(req,file,cb) {
-        
-        cb(null, new Date().toDateString() + '-'+ file.originalname);
-    }
-})
 
-const fileFilter = function(req,file,cb) {
-    
-    if(file.mimetype === 'image/png' || 
-    file.mimetype === 'image/jpg' ||
-     file.mimetype === 'image/jpeg'){
-         cb(null,true)
-     }
-     else cb(null,false)
-}
 
 app.use(cors());
 
@@ -41,7 +23,7 @@ app.use((req,res,next) => {
 })
 app.use(bodyParser.json());
 
-app.use(multer({storage:fileStorage,fileFilter:fileFilter}).single('image'));
+
 
 //make path publicly available
 app.use('/images',express.static(path.join(__dirname,'images')));
